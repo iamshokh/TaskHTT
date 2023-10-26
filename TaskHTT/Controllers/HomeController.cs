647da3ea8d22a9e94b7d6ed62b39a56f7758623c
iamshokh <shaxzodturakulov0880@gmail.com>
2023-10-26 21:15:56 +0500

    add changes

--- TaskHTT/Controllers/HomeController.cs
+++ TaskHTT/Controllers/HomeController.cs
@@ -1,7 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
-using TaskHTT.Models;
-using TaskHTT.ServiceLayer.CategoryService;
+using TaskHTT.Web.Models;
 
 namespace TaskHTT.Web.Controllers
 {
